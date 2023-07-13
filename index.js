8af152eb70f62777577b79999a1cea7370c23712
Hussein Saidi <callmethegeek19@gmail.com>
2023-07-13 15:18:49 +0200

    second day of asking

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
-const puppeteer = require('puppeteer');
 const express = require('express');
-
-//initialisation
+// const cheerio = require('cheerio');
+// const axios = require('axios');
+const puppeteer = require('puppeteer');
+const PORT = 8080;
 const app = express();
-const PORT = 8000;
+
 
 const scrap = async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
-  await page.goto('https://quotes.toscrape.com/');
+  await page.goto('https://books.toscrape.com/');
+
   // Set screen size
   await page.setViewport({width: 1080, height: 1024});
-  
-  const result = await page.$eval('div.col-md-4.tags-box span.tag-item', function(quotes) {
-      return quotes.innerText;
-  });
+   
+  const result = await page.evaluate(() => Array.from(document.querySelectorAll('ul.nav.nav-list li ul li a'), (item) => {
+       return {
+         bookName: item.innerText,
+         bookLink: item.getAttribute('href')
+       }
+  }));
 
-  console.log(result)
-  return result;
+  // console.log(result);
+  
   await browser.close();
+  return result;
 };
 
-scrap();
-
 app.get('/', async (req,res) => {
-    const data = await scrap();
-    res.json(data);
+   const result = await scrap();
+   console.log(result);
+   res.json(result);
 });
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
 
 
-// const puppeteer = require('puppeteer');
-// const express = require('express');
 
-// //initialisation
-// const app = express();
-// const PORT = 8000;
+// axios('https://books.toscrape.com/').then(response => {
+//   const html = response.data;
+//   const $ = cheerio.load(html);
+//   const data = [];
+//    $('ul.nav.nav-list li a', html).each(() => {
+//      const result = $(this).text();
+//      data.push({
+//         result
+//      })
+//  })
+//   console.log(data.length)
+// }).catch(e => console.error(e))
 
-// app.get('/', async (req,res) => {
-//     const browser = await puppeteer.launch({headless: false});
-//     const page = await browser.newPage();
-  
-//     await page.goto('https://www.imdb.com/chart/top/');
-//     // Set screen size
-//     await page.setViewport({width: 1080, height: 1024});
-  
-//     const result = await page.$$eval('h1', );
-//     res.json(result)
-    
-//     await browser.close();
-//   })
-
-// app.listen(PORT, () => console.log(`server running on port ${PORT}`))
